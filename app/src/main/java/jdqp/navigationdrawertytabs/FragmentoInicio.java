de02daaf4437b423ee510d5dab7fdc0e8fa30404
jhonatanquilca <jhonatand.quilca@gmail.com>
2015-12-19 18:07:16 -0500

    drawer layout (menu deracha principal)

---
+++ app/src/main/java/jdqp/navigationdrawertytabs/FragmentoInicio.java
@@ -0,0 +1,47 @@
+package jdqp.navigationdrawertytabs;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jdqp.navigationdrawertytabs.ui.AdaptadorInicio;
+
+
+public class FragmentoInicio extends Fragment {
+
+
+    private RecyclerView reciclador;
+    private LinearLayoutManager layoutManager;
+    private AdaptadorInicio adaptador;
+
+    public FragmentoInicio() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragmento_inicio, container, false);
+
+        reciclador = (RecyclerView) view.findViewById(R.id.reciclador);
+        layoutManager = new LinearLayoutManager(getActivity());
+        reciclador.setLayoutManager(layoutManager);
+
+        adaptador = new AdaptadorInicio();
+        reciclador.setAdapter(adaptador);
+        return view;
+
+
+    }
+}
--- app/src/main/java/jdqp/navigationdrawertytabs/MainActivity.java
+++ app/src/main/java/jdqp/navigationdrawertytabs/MainActivity.java
@@ -1,16 +1,91 @@
 package jdqp.navigationdrawertytabs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    //    todo el contenido
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        agregarToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            prepararDrawer(navigationView);
+            // Seleccionar item por defecto
+            seleccionarItem(navigationView.getMenu().getItem(0));
+        }
+
+    }
+
+    private void prepararDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        seleccionarItem(menuItem);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+    }
+
+    private void seleccionarItem(MenuItem itemDrawer) {
+        Fragment fragmentoGenerico = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch (itemDrawer.getItemId()) {
+            case R.id.item_inicio:
+                fragmentoGenerico = new FragmentoInicio();
+                break;
+            case R.id.item_cuenta:
+                // Fragmento para la sección Cuenta
+                break;
+            case R.id.item_categorias:
+                // Fragmento para la sección Categorías
+                break;
+            case R.id.item_configuracion:
+                // Iniciar actividad de configuración
+                break;
+        }
+        if (fragmentoGenerico != null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.contenedor_principal, fragmentoGenerico)
+                    .commit();
+        }
+
+        // Setear título actual
+        setTitle(itemDrawer.getTitle());
+    }
+
+    private void agregarToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.drawer_toggle);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 
     @Override
